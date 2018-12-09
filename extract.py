4fef682ce41510ed2e8c9aa911e20be8d030ae25
Simon Wong <simon.wonng@gmail.com>
2018-12-09 17:39:49 -0500

    Fixed extract.py for Unix filesystems

--- extract.py
+++ extract.py
@@ -4,33 +4,36 @@ from pathlib import Path
 
 """
 This script should be placed in the folder that contains folders with chat names (and chat JSON files within).
-Currently as of December 12, 2018 the correct folder would be "{extract_location}/messages/inbox"
+Currently as of December 2018 the correct folder would be "{extract_location}/messages/inbox"
 """
 
-archive = Path(os.getcwd() + "\\archive\\")
+archive = Path(os.getcwd()) / "archive"
 root = Path(os.getcwd())
 
 files = os.listdir()
 
 # for backwards compatability with older facebook extracts
 try:
-    sticker_folder = files.index("stickers_used")
-    del files[sticker_folder]
-    shutil.move("stickers_used", archive)
+	sticker_folder = files.index("stickers_used")
+	del files[sticker_folder]
+	shutil.move("stickers_used", archive)
 except:
-    pass
+	pass
 
 files = [x for x in files if os.path.isdir(x)]
 
 
 if not os.path.exists(archive):
-    os.mkdir(archive)
+	os.mkdir(archive)
 
 for file in files:
-    try:
-        os.chdir(file)
-        os.rename("message.json", str(root / (file + ".json")))
-        os.chdir("..")
-        os.rename(file, archive / file)
-    except:
-        print("skipped", file)
+	try:
+		os.chdir(file)
+		if "message.json" in os.listdir():
+			os.rename("message.json", str(root / (file + ".json")))
+		os.chdir("..")
+		os.mkdir(archive / file)
+		os.rename(file, archive / file)
+	except:
+		print("skipped folder due to error:", file)
+		os.chdir(root)
